64950680f7c773957924b27a32aac4a032960cea
pratik252004-proton <protonx252004@gmail.com>
2026-08-01 13:53:50 +0530

    Production path added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://client-complaint-management-system.onrender.com',
         changeOrigin: true
       }
     }
